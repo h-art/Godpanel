627799349bf48a60bbcce57f9cb234ad16397d75
Matteo Rosati <mrosati@h-art.com>
2016-04-26 21:21:36 +0200

    file format

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,12 @@
 from fabric.api import local
 
+
 def test():
-  local("./manage.py test")
+    local("./manage.py test")
+
 
 def build():
-  local("./manage.py reset_db --noinput")
-  local("./manage.py migrate")
-  local("./manage.py sqlflush | ./manage.py dbshell")
-  local("./manage.py loaddata initial_data")
+    local("./manage.py reset_db --noinput")
+    local("./manage.py migrate")
+    local("./manage.py sqlflush | ./manage.py dbshell")
+    local("./manage.py loaddata initial_data")
