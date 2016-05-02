6d308fcbcc29b344518f95eb7db735cdf519678f
Matteo Rosati <mrosati@h-art.com>
2016-05-02 11:21:39 +0200

    refactor configuration to use ini config file

--- hart/wsgi.py
+++ hart/wsgi.py
@@ -7,10 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
+import os, configparser
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hart.settings.local")
+parser = configparser.ConfigParser()
+
+try:
+    parser.read_file(open('.config.ini'))
+    django_settings_module = parser.get('wsgi', 'django_settings_module')
+except FileNotFoundError:
+    django_settings_module = 'hart.settings.local'
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", django_settings_module)
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
-import os
-import sys
+import os, sys, configparser
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hart.settings.local")
+    parser = configparser.ConfigParser()
+
+    try:
+        parser.read_file(open('.config.ini'))
+        django_settings_module = parser.get('wsgi', 'django_settings_module')
+    except FileNotFoundError:
+        django_settings_module = 'hart.settings.local'
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", django_settings_module)
 
     from django.core.management import execute_from_command_line
 
