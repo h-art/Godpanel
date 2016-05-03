202d19f7d6665707f447281ba8b854aee3008624
Matteo Rosati <mrosati@h-art.com>
2016-05-03 10:05:04 +0200

    remove ini based configuration. add separated wsgi per-env file

--- manage.py
+++ manage.py
@@ -1,19 +1,9 @@
 #!/usr/bin/env python
 import os
 import sys
-import configparser
 
 if __name__ == "__main__":
-    parser = configparser.ConfigParser()
-
-    try:
-        filename = os.path.realpath(os.path.join(os.path.dirname(__file__), '.config.ini'))
-        parser.read_file(open(filename))
-        django_settings_module = parser.get('wsgi', 'django_settings_module')
-    except FileNotFoundError:
-        django_settings_module = 'hart.settings.local'
-
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", django_settings_module)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hart.settings.local")
 
     from django.core.management import execute_from_command_line
 
