43b2d3b22f7701912d8f1f7d4457a72b09cf28ef
Matteo Rosati <mrosati@h-art.com>
2016-05-25 08:36:24 +0200

    add function to serialise forms to objects

--- godpanel/static/godpanel/js/godpanel.js
+++ godpanel/static/godpanel/js/godpanel.js
@@ -1,4 +1,19 @@
-$(function () {
+(function ($) {
+  $.fn.serializeObject = function() {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+      if (o[this.name] !== undefined) {
+        if (!o[this.name].push) {
+          o[this.name] = [o[this.name]];
+        }
+        o[this.name].push(this.value || '');
+      } else {
+        o[this.name] = this.value || '';
+      }
+    });
+    return o;
+  };
   /**
    * Get a cookie from the browser
    * @param  {string} name The cookie name
